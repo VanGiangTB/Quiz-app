e96e2fc17bccf11ae3223708c7c9b48ea234c9d2
VanGiangTB <vangiang97.tb@gmail.com>
2021-08-11 22:55:27 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,47 +1,98 @@
-import logo from './logo.svg'
-import './App.css';
-import React, {useState, useEffect} from 'react'
-
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import questions from "./Question.json";
+import classNames from "classnames"
 
 function App() {
-  const [question, setQuestion] = useState([]);
+  let [questionNumber, setQuestionNumber] = useState(0);
+  const [question, setQuestion] = useState(() => questions[questionNumber]);
+  const [isCorrect, setIsCorrect] = useState(false);
+  const [isSelect, setIsSelect] = useState(false);
 
-  // useEffect(() =>{
-  //   fetch(API_URL)
-  //   .then(res => res.json())
-  //   .then(data =>)
-  // },[] )
+  useEffect(() => {
+    setQuestion(questions[questionNumber]);
+  }, [questionNumber]);
+
+  const handleNext = () => {
+    setIsSelect(false)
+    setIsCorrect(false)
+    setQuestionNumber((questionNumber += 1));
+   
+  };
+
+  const checkAnswer = (answer) => {
+    console.log("answer", answer);
+    setIsSelect(true);
+    if (answer === question.correct_answer) {
+      setIsCorrect(true);
+      // correctClass()
+    } else {
+      setIsCorrect(false);
+    }
+  };
+
+  // useEffect(() => {
+  //   correctClass()
+  // }, [isCorrect])
+
+  const correctClass = () => {
+    if (isCorrect) {
+      return "bg-green-400"
+    }
+    return ""
+  }
 
   return (
     <div className="App grid grid-cols-6">
-      <div className='col-start-2 col-span-4'>
-        <div className='progress-bar bg-gray-400 h-8 m-0 p-0 w-2/5'></div>
-        <div className='question-wrapper py-5 px-24'>
-          <p className='question-number text-2xl text font-bold'>
-            Question 1 of 20
+      <div className="col-start-2 col-span-4">
+        <div className="progress-bar bg-gray-400 h-8 m-0 p-0 w-2/5"></div>
+
+        <div className="question-wrapper py-5 px-24">
+          <p className="question-number text-2xl text font-bold">
+            Question <span>{question.title}</span> of 20
           </p>
-          <p className='question-category text-base text-gray-400 my-2'>
-            Animals
+          <p className="question-category text-base text-gray-400 my-2">
+            {question.category}
           </p>
-          <p className='question-difficulty text-sm my-1 text-gray-400'>
-          ★✰✰
+          <p className="question-difficulty text-sm my-1 text-gray-400">
+            {question.difficulty}
           </p>
-          <p className='question-text text-xl font-bold mt-2'>Kangaroos keep food in their pouches next to their children ?</p>
+          <p className="question-text text-xl font-bold mt-2">
+            {question.question}
+          </p>
         </div>
-        <div className='answer-wrapper py-5 px-24 justify-between flex flex-wrap mt-4 '>
-          <button className='answer-button  p-2 rounded-md bg-gray-400 font-medium text-lg mb-5 border-black border w-2/5 '>Homo</button>
-          <button className='answer-button answer-button  p-2 rounded-md bg-gray-400 font-medium text-lg mb-5 border-black border w-2/5 '>Homo</button>
-          <button className='answer-button answer-button  p-2 rounded-md bg-gray-400 font-medium text-lg mb-5 border-black border w-2/5 '>Homo</button>
-          <button className='answer-button answer-button  p-2 rounded-md bg-gray-400 font-medium text-lg mb-5 border-black border w-2/5'>Homo</button>
+        <div className="answer-wrapper py-5 px-24 justify-between flex flex-wrap mt-4 ">
+          {question.answers.length &&
+            question.answers.map((answer, index) => {
+              return (
+                <button
+                  key={index}
+                  className={classNames('answer-button answer-button  p-2 rounded-md bg-gray-400 hover:bg-gray-500 font-medium text-lg mb-5 border-black border w-2/5', correctClass())}
+                  onClick={() => checkAnswer(answer)}
+                >
+                  {answer}
+                </button>
+              );
+            })}
         </div>
-        <p className='text-2xl font-bold text-center'>Wrong Answer</p>
-        <div className='flex justify-center mt-2'>
-          <button className='px-7 py-2 rounded-md bg-gray-400 text-lg font-medium border border-black'>
-            Next Question
-          </button>
+
+        {isSelect && (
+          <>
+            <p className="text-2xl font-bold text-center">
+              {isCorrect ? "Correct" : "Wrong"} Answer
+            </p>
+            <div className="flex justify-center mt-2">
+              <button
+                className="px-7 py-2 rounded-md bg-gray-400 text-lg font-medium border border-black"
+                onClick={handleNext}
+              >
+                Next Question
+              </button>
+            </div>
+          </>
+        )}
       </div>
-    </div> 
-  </div>
+    </div>
   );
 }
 
